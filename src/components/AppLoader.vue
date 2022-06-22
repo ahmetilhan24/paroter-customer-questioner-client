1a5131380d3e568d6b864ee05f113f77e86a7e4c
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-06-23 00:16:35 +0300

    Test added

--- src/components/AppLoader.vue
+++ src/components/AppLoader.vue
@@ -11,7 +11,7 @@
 <script lang="js">
 import { mapState } from 'vuex';
 export default {
-  name: "AppPreloder",
+  name: "AppLoader",
   computed:  {
     ...mapState({
       isLoaderVisible: store => store.modals.isLoaderVisible
